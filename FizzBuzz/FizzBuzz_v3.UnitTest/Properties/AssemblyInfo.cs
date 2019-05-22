f6305f0fbd5ba7a665b46e3eee2a1b2ad5af1f1b
Raymond Tukkers <rt@searchhappens.eu>
2019-05-22 09:33:25 +0200

    Added Unit tests for the fizzbuzz factory.

---
+++ FizzBuzz/FizzBuzz_v3.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz_v3.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBuzz_v3.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a3f8fb5-2f32-415c-afa2-bd3117786311")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FizzBuzz/FizzBuzz_v3.UnitTest/UnitTestFizzBuzz.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FizzBuzz_v3.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz_v3.UnitTest
+{
+    [TestClass]
+    public class UnitTestFizzBuzz
+    {
+        [TestMethod]
+        public void TestFizzBuzzValidValues()
+        {
+            var fizzBuzzFactory = FizzBuzzFactory.Instance;
+            Assert.AreEqual("1", fizzBuzzFactory.DetermineFuzziness(1));
+            Assert.AreEqual("2", fizzBuzzFactory.DetermineFuzziness(2));
+            Assert.AreEqual("Fizz", fizzBuzzFactory.DetermineFuzziness(3));
+            Assert.AreEqual("4", fizzBuzzFactory.DetermineFuzziness(4));
+            Assert.AreEqual("Buzz", fizzBuzzFactory.DetermineFuzziness(5));
+
+            // 6 is a multiple of 3, which of course should output Fizz.
+            // uncommenting this line will cause unit tests to fail
+            // Assert.AreEqual("6", fizzBuzzFactory.DetermineFuzziness(6));
+
+            Assert.AreEqual("Fizz", fizzBuzzFactory.DetermineFuzziness(6));
+
+            // skip uniteresting numbers, can add a test case for this if any bugs occur.
+            Assert.AreEqual("14", fizzBuzzFactory.DetermineFuzziness(14));
+            Assert.AreEqual("FizzBuzz", fizzBuzzFactory.DetermineFuzziness(15));
+
+            // latest 'fizzbuzz' possible when <= 100
+            Assert.AreEqual("FizzBuzz", fizzBuzzFactory.DetermineFuzziness(90));
+
+            // latest 'numeric value' possible when <= 100
+            Assert.AreEqual("98", fizzBuzzFactory.DetermineFuzziness(98));
+
+            // latest 'fizz' possible
+            Assert.AreEqual("Fizz", fizzBuzzFactory.DetermineFuzziness(99));
+
+            // latest 'buzz' possible when <= 100
+            Assert.AreEqual("Buzz", fizzBuzzFactory.DetermineFuzziness(100));
+        }
+
+        [TestMethod]
+        [Description("Make sure the FizzBuzzFactory can handle unexpected values (gracefully) as well.")]
+        public void TestFizzBuzzUnexpectedValues()
+        {
+            var fizzBuzzFactory = FizzBuzzFactory.Instance;
+            Assert.AreEqual("Fizz", fizzBuzzFactory.DetermineFuzziness(999));
+
+
+        }
+    }
+}
